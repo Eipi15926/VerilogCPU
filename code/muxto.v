79a498bbecb34765d8497a60696d6f14cefe0ad9
RollingYoyoball <5958628@qq.com>
2022-12-02 15:53:48 +0800

    change route

--- code/muxto.v
+++
@@ -1,9 +0,0 @@
-module muxtwo (out,a,b,sl);
-    input a,b,sl;
-    output out;
-
-    not u1(nsl,sl);
-    and #1 u2(sela,a,nsl);
-    and #1 u3(selb,b,sl);
-    or #1 u4(out,sela,selb);
-endmodule
