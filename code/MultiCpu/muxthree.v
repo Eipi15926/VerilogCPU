748dded428136ff4b3e79f76e7252b6ab7434582
RollingYoyoball <5958628@qq.com>
2023-02-12 22:37:22 +0800

    upload mux3 and mux4

---
+++ code/MultiCpu/muxthree.v
@@ -0,0 +1,14 @@
+module muxthree(I0,I1,I2,ctrl,out);
+    parameter width=32;
+    input[width-1:0] I0,I1,I2;
+    input[2-1:0] ctrl;
+    output reg[width-1:0] out;
+
+    always@(I0 or I1 or I2 or ctrl) begin
+        case(ctrl)
+            2'b00:out=I0;
+            2'b01:out=I1;
+            2'b10:out=I2;
+        endcase
+    end
+endmodule
